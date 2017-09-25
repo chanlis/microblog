821c717ca4d7a80388ae898ad2a283ffe4036e78
Lisa <lisachan.nb@gmail.com>
2017-09-25 15:15:06 -0400

    first commit

---
+++ lib/microblog/user/message.ex
@@ -0,0 +1,20 @@
+defmodule Microblog.User.Message do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Microblog.User.Message
+
+
+  schema "messages" do
+    field :msg, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Message{} = message, attrs) do
+    message
+    |> cast(attrs, [:name, :msg])
+    |> validate_required([:name, :msg])
+  end
+end
